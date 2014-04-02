e557249c24fb5332870809028830dd66a241bd7f
Sara Naeem <naeem_sara@bah.com>
2014-04-01 21:03:10 -0400

    added books index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,32 @@ class BooksController < ApplicationController
 def show
   @book = Book.find(params[:id])
 end
+def new
+  @book = Book.new
+end
+
+def create
+  @book = Book.new(book_params)
+  @book.save
+  redirect_to @book
+end
+
+def edit
+  @book = Book.find(params[:id])
+end
+
+def update
+  @book = Book.find(params[:id])
+  @book.update(book_params)
+  redirect_to @book
+end
+
+
+
+private
+
+def book_params
+  params.require(:book).permit(:title, :author, :pages, :price)
+end
+
 end
