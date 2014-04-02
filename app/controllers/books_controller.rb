f71f9e74da3a1c7bd836dbb881e98743069816a3
Sara Naeem <naeem_sara@bah.com>
2014-04-01 21:34:42 -0400

    extracted partials for form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,37 +1,45 @@
 class BooksController < ApplicationController
+	
+	before_action :set_book, only: [ :show, :edit, :update, :destroy ]
+
+
 	def index
 		@available_at = Time.now
 		@books = Book.all
 	end
 def show
-  @book = Book.find(params[:id])
+
 end
 def new
   @book = Book.new
 end
 
 def create
-  @book = Book.new(book_params)
-  @book.save
+   @book.save
   redirect_to @book
 end
 
 def edit
-  @book = Book.find(params[:id])
+
 end
 
 def update
-  @book = Book.find(params[:id])
+
   @book.update(book_params)
   redirect_to @book
 end
 
 
-
+def destroy
+   @book.destroy
+  redirect_to books_url
+end
 private
 
 def book_params
   params.require(:book).permit(:title, :author, :pages, :price)
 end
-
+def set_book
+  @book = Book.find(params[:id])
+end
 end
